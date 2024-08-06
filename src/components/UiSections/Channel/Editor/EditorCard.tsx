35a6a3abcb4aeed252ec776e2ddd22bac90610f1
Akash Sharma <aakash6263264@gmail.com>
2024-08-06 18:25:59 +0530

    Removed User Button & Added PopUp

--- src/components/UiSections/Channel/Editor/EditorCard.tsx
+++ src/components/UiSections/Channel/Editor/EditorCard.tsx
@@ -1,14 +1,39 @@
 import Button from "@/components/Buttons/Button"
+import PopUpModal from "@/components/Modal/PopUpModal"
 import { UserBasicDetailsType } from "@/utils/types/user"
 import Image from "next/image"
-import React from "react"
+import React, { useState } from "react"
 import { RiDeleteBack2Line } from "react-icons/ri"
 
 function EditorCard({ editor }: { editor: UserBasicDetailsType }) {
-    console.log(editor.image)
-
+    console.log(editor?.image)
+    const [isOpen, setIsOpen] = useState<boolean>(false);
     return (
         <div className="w-full flex items-center justify-between h-24 bg-secondary-hover px-4 rounded-xl">
+            <PopUpModal
+                isOpen={isOpen}
+                closeModal={() => {
+                    setIsOpen(false)
+                }}
+            >
+                <div className="w-full flex justify-start items-center flex-col p-3">
+                    <div className="flex justify-center items-center">
+                        Are you sure you want to remove this Editor?
+                    </div>
+                    <div className="flex justify-between items-center text-white w-full">
+                        <Button
+                            className="p-2 text-sm md:text-base"
+                            text="No"
+                            onClick={() => setIsOpen(false)}
+                        />
+                        <Button
+                            className="p-2 bg-green-400 border-none hover:bg-green-500 text-sm md:text-base"
+                            text="Yes"
+                        />
+                    </div>
+                </div>
+            </PopUpModal>
+
             <div className="flex items-center justify-center gap-2">
                 <Image
                     width={60}
@@ -29,6 +54,7 @@ function EditorCard({ editor }: { editor: UserBasicDetailsType }) {
                 icon={<RiDeleteBack2Line />}
                 text="remove"
                 className="btn_1_2"
+                onClick={() => setIsOpen(true)}
             />
         </div>
     )
